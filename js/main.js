c63a6140af5f5d7a904200b2f308723780c47500
yahyojondev <nabijonovdeveloper@gmail.com>
2023-12-06 14:35:02 +0500

    exam-2.1

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    nav:true, 
+    items:4,
+    navText:['<img src="images/left.svg"/>','<img src="images/right.svg"/>'],
+    dots:true,
+    // autoplay:true,
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        900:{
+            items:3,
+        },
+        1200:{
+            items:4
+        },
+    }
+  });
+});
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
